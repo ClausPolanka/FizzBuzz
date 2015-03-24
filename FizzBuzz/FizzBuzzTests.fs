ea6ec428fadf1d2e363b634790f1f4b28149c798
Claus Polanka <claus.polanka@chello.at>
2015-03-24 15:25:51 +0100

    [Red] Initial commit including first test which fails.

---
+++ FizzBuzz/FizzBuzzTests.fs
@@ -0,0 +1,17 @@
+﻿module FizzBuzz
+
+open Xunit
+open Xunit.Extensions
+open Swensen.Unquote
+
+module FizzBuzz =
+    let transform number = ""
+
+module Tests =
+    [<Theory>]
+    [<InlineData(4)>]
+    let ``FizzBuzz.transform returns the number`` (number : int) = 
+        let actual = FizzBuzz.transform number
+        let expected = string number
+        test <@ expected = actual @>
+
