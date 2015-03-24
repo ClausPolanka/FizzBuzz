39083b3182199847949f350f692f0f575f83426c
Claus Polanka <claus.polanka@chello.at>
2015-03-24 15:37:35 +0100

    [Red] FizzBuzz transforms 9 to Fizz.

--- FizzBuzz/FizzBuzzTests.fs
+++ FizzBuzz/FizzBuzzTests.fs
@@ -23,6 +23,7 @@ module Tests =
     
     [<Theory>]
     [<InlineData(3)>]
+    [<InlineData(9)>]
     let ``FizzBuzz.transform returns Fizz`` (number : int) = 
         let actual = FizzBuzz.transform number
         let expected = "Fizz"
